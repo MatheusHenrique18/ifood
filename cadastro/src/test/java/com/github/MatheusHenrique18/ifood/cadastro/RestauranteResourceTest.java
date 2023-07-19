aaa366ef71981688ea1ca0d4e3082e9469089afc
MatheusHenrique18 <matheus.hsb18@gmail.com>
2023-07-18 21:31:05 -0300

    criando teste para alteração de restaurantes e excluindo pasta data.

--- cadastro/src/test/java/com/github/MatheusHenrique18/ifood/cadastro/RestauranteResourceTest.java
+++ cadastro/src/test/java/com/github/MatheusHenrique18/ifood/cadastro/RestauranteResourceTest.java
@@ -2,7 +2,10 @@ package com.github.MatheusHenrique18.ifood.cadastro;
 
 import static io.restassured.RestAssured.given;
 
+import javax.ws.rs.core.Response.Status;
+
 import org.approvaltests.JsonApprovals;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import com.github.database.rider.cdi.api.DBRider;
@@ -12,6 +15,7 @@ import com.github.database.rider.core.api.dataset.DataSet;
 
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
 
 @DBRider
 @DBUnit(caseInsensitiveStrategy = Orthography.LOWERCASE)
@@ -28,6 +32,27 @@ public class RestauranteResourceTest {
 		          .statusCode(200)
 		          .extract().asString();
         JsonApprovals.verifyJson(resultado);
+    }
+    
+    @Test
+    @DataSet("restaurantes-cenario-1.yml")
+    public void testAlterarRestaurante() {
+    	Restaurante dto = new Restaurante();
+    	dto.nome = "novo nome";
+    	Long parameterValue = 123L;
+    	
+    	given()
+    	.contentType(ContentType.JSON)
+    	.with().pathParam("id", parameterValue)
+    	.body(dto)
+    	.when().put("/restaurantes/{id}")
+    	.then()
+    	.statusCode(Status.NO_CONTENT.getStatusCode());
+    	
+    	Restaurante restaurante = Restaurante.findById(parameterValue);
+    	
+    	Assert.assertEquals(dto.nome, restaurante.nome);
+    	
     }
 
 }
