2fbd4299299660614c1afb87e0e53a39a2006917
MatheusHenrique18 <matheus.hsb18@outlook.com>
2023-09-24 20:57:11 -0300

    configurando e criando primeiro endpoint reativo.

--- marketplace/src/main/java/com/github/MatheusHenrique18/ifood/mp/Prato.java
+++ marketplace/src/main/java/com/github/MatheusHenrique18/ifood/mp/Prato.java
@@ -1,6 +1,13 @@
 package com.github.MatheusHenrique18.ifood.mp;
 
 import java.math.BigDecimal;
+import java.util.stream.StreamSupport;
+
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+import io.vertx.mutiny.pgclient.PgPool;
+import io.vertx.mutiny.sqlclient.Row;
+import io.vertx.mutiny.sqlclient.RowSet;
 
 public class Prato {
 
@@ -9,5 +16,12 @@ public class Prato {
 	public String descricao;
 	public Restaurante restaurante;
 	public BigDecimal preco;
+	
+	public static Multi<PratoDTO> findAll(PgPool pgPool) {
+		Uni<RowSet<Row>> preparedQuery = pgPool.preparedQuery("select * from prato").execute();
+		return preparedQuery.onItem().transformToMulti(rowSet -> Multi.createFrom().items(() -> {
+			return StreamSupport.stream(rowSet.spliterator(), false);
+		})).onItem().transform(PratoDTO::from);
+	}
 	
 }
