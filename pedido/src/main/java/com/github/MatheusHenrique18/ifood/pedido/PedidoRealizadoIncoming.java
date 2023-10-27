a355102665f12d1a890f700686eba36a8b7c050a
MatheusHenrique18 <matheus.hsb18@outlook.com>
2023-10-27 19:24:46 -0300

    persistindo pedidos recebidos

--- pedido/src/main/java/com/github/MatheusHenrique18/ifood/pedido/PedidoRealizadoIncoming.java
+++ pedido/src/main/java/com/github/MatheusHenrique18/ifood/pedido/PedidoRealizadoIncoming.java
@@ -1,10 +1,14 @@
 package com.github.MatheusHenrique18.ifood.pedido;
 
+import java.util.ArrayList;
+
 import javax.enterprise.context.ApplicationScoped;
 
+import org.bson.types.Decimal128;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
 import com.github.MatheusHenrique18.ifood.pedido.dto.PedidoRealizadoDTO;
+import com.github.MatheusHenrique18.ifood.pedido.dto.PratoPedidoDTO;
 
 @ApplicationScoped
 public class PedidoRealizadoIncoming {
@@ -13,5 +17,22 @@ public class PedidoRealizadoIncoming {
 	public void lerPedidos(PedidoRealizadoDTO dto) {
 		System.out.println("*-----------------------*");
 		System.out.println(dto);
+		
+		Pedido p = new Pedido();
+        p.cliente = dto.cliente;
+        p.pratos = new ArrayList<>();
+        dto.pratos.forEach(prato -> p.pratos.add(from(prato)));
+        Restaurante restaurante = new Restaurante();
+        restaurante.nome = dto.restaurante.nome;
+        p.restaurante = restaurante;
+        p.persist();
 	}
+	
+    private Prato from(PratoPedidoDTO prato) {
+        Prato p = new Prato();
+        p.descricao = prato.descricao;
+        p.nome = prato.nome;
+        p.preco = new Decimal128(prato.preco);
+        return p;
+    }
 }
